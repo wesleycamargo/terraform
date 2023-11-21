75877849db140b508c03da02feffefbd9a2afcb6
Wesley Camargo <wesleycamargo@outlook.com>
2023-11-21 19:12:00 +0100

    added iis vm, shutdown vm

--- iis-vm/iis-vm.tf
+++ iis-vm/iis-vm.tf
@@ -9,7 +9,7 @@ variable "resource_group_location" {
 
 variable "prefix" {
   type        = string
-  default     = "iis-wes2"
+  default     = "iis"
   description = "Prefix of the resource name"
 }
 
@@ -60,7 +60,7 @@ resource "azurerm_network_interface" "nic" {
 resource "azurerm_windows_virtual_machine" "main" {
   name                  = "vm-${var.prefix}"
   admin_username        = "azureuser"
-  admin_password        = "Password1234!"
+  admin_password        = "@Zurepassw0rd"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
@@ -78,6 +78,21 @@ resource "azurerm_windows_virtual_machine" "main" {
     sku       = "2022-datacenter-azure-edition"
     version   = "latest"
   }
+}
+
+# Configure IIS and create a simple html page
+resource "azurerm_virtual_machine_extension" "install-iis" {
+  name                 = "${var.prefix}-wsi"
+  virtual_machine_id   = azurerm_windows_virtual_machine.main.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "powershell -encodedCommand ${textencodebase64(file("${path.module}/configure-iis.ps1"), "UTF-16LE")}"
+    }
+  SETTINGS
 }
 
 resource "azurerm_dev_test_global_vm_shutdown_schedule" "example" {
@@ -99,4 +114,9 @@ output "resource_group_name" {
 
 output "public_ip_address" {
   value = azurerm_windows_virtual_machine.main.public_ip_address
+}
+
+output "admin_password" {
+  sensitive = true
+  value     = azurerm_windows_virtual_machine.main.admin_password
 }
